a8271709b86511ad213868d2478e4360c680615a
Damaris20077 <davilez@estud.tesa.edu.ec>
2026-07-24 22:07:47 -0500

    Primera version pagina CST

---
+++ script.js
@@ -0,0 +1,12 @@
+function contactar(){
+
+alert("Te enviaremos a WhatsApp para solicitar tu reparación 📱");
+
+}
+
+
+function mensaje(){
+
+alert("Gracias por contactarnos. Responderemos pronto.");
+
+}
