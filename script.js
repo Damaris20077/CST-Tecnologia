0c206ff713d0f4ff410c11de1af956b269243c30
Damaris20077 <davilez@estud.tesa.edu.ec>
2026-07-28 20:32:54 -0500

    quitar cuadros de marcas del catalogo

--- script.js
+++ script.js
@@ -10,15 +10,11 @@ const celulares = {
         "Galaxy S25"
     ],
 
+
     Xiaomi: [
         "Redmi 14C",
         "Redmi Note 14 Pro",
         "Redmi Note 14"
-    ],
-
-    POCO: [
-        "POCO X7 Pro",
-        "POCO X7"
     ],
 
     Motorola: [
@@ -27,11 +23,13 @@ const celulares = {
         "Moto G75"
     ],
 
+
     Honor: [
         "Honor X6c",
         "Honor X8c",
         "Honor 400 Lite"
     ],
+
 
     Apple: [
         "iPhone 13",
@@ -42,56 +40,79 @@ const celulares = {
 
 
 
+
 // CAMBIAR MODELOS SEGÚN MARCA
+
 
 const marca = document.getElementById("marca");
 const modelo = document.getElementById("modelo");
 
 
+
 if(marca && modelo){
+
 
     marca.addEventListener("change", function(){
 
+
         let marcaSeleccionada = this.value;
+
 
 
         modelo.innerHTML =
         '<option value="">Selecciona un modelo</option>';
 
 
+
         if(celulares[marcaSeleccionada]){
 
+
             celulares[marcaSeleccionada].forEach(function(nombre){
+
 
 
                 let opcion = document.createElement("option");
 
 
+
                 opcion.value = nombre;
 
+
                 opcion.textContent = nombre;
+
 
 
                 modelo.appendChild(opcion);
 
 
+
             });
+
 
         }
 
 
+
     });
+
+
 
 }
 
 
 
+
+
+
 // MENSAJE AL ENVIAR SOLICITUD
+
 
 const formulario = document.getElementById("formulario");
 
 
+
 if(formulario){
+
 
     formulario.addEventListener("submit", function(e){
 
@@ -99,13 +120,23 @@ if(formulario){
         e.preventDefault();
 
 
+
         document.getElementById("mensajeExito").innerHTML =
-        "✅ Solicitud enviada correctamente. Un asesor se comunicará contigo.";
+
+        "✅ ¡Solicitud enviada correctamente! <br><br>" +
+
+        "Gracias por confiar en CST Servicio & Tecnología. " +
+
+        "Hemos informado a nuestro asesor y se comunicará contigo al número 📞 0962055495 para ayudarte con tu compra.";
+
 
 
         formulario.reset();
 
 
+
     });
+
+
 
 }
